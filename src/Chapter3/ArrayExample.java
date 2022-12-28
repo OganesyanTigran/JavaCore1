3df2baef0213c5c5879b601631e0600cfb6d2b98
tigran <oganesyantigran1998@gmail.com>
2022-12-28 10:03:17 -0800

    first commit

---
+++ src/Chapter3/ArrayExample.java
@@ -0,0 +1,26 @@
+package Chapter3;
+
+public class ArrayExample {
+
+   public static void main(String[] args){
+
+        int[] myArray = new int[5];
+
+
+           myArray[0] = 8;
+           myArray[1] = 9;
+           myArray[2] = 10;
+           myArray[3] = 4043;
+           myArray[4] = -3;
+
+      for (int i = 0; i <5 ; i++) {
+         System.out.println(myArray[i]);
+
+      }
+
+      }
+
+
+
+
+        }
