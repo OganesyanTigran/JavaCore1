031af0d9889dad570f56e4c903f0d5e8358c57cb
tigran <oganesyantigran1998@gmail.com>
2022-12-28 10:04:54 -0800

    first commit

---
+++ src/Class/For/ArrayExample.java
@@ -0,0 +1,12 @@
+package Class.For;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] array = {4, 6, 8, 10, 9, -4, 21};
+//        System.out.println(array[0]);
+  //      for (int i = array.length-1; i >= 0; i--) {
+   //         System.out.print(array[i]+ " ");
+        //  }
+        System.out.println(array[array.length-1]);
+    }
+}
